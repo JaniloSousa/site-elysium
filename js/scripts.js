e1de0c66b841e2b917a315d88ed55a3e519b8a95
Janilo Paixão de Sousa <janilopaixao5@gmail.com>
2023-11-09 11:16:49 -0200

    projeto concluido

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 window.sr = ScrollReveal({ reset:true });
 
 sr.reveal(
-    'section#horizontal-cards div.horizontal-cards div.horizontal-card',
-    { duration: 3000});
+    '.reveal',
+    { duration: 2000,});
